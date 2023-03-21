60519b0b85c024836d60516c275e8804c88c8165
Catania Peter <peter.catania@student.supsi.ch>
2023-03-21 12:06:01 +0100

    add basic next tesk view

--- Expo-FaMS-Labelling/screens/NextTaskScreen.js
+++ Expo-FaMS-Labelling/screens/NextTaskScreen.js
@@ -1,14 +1,71 @@
 // External imports
 import React from 'react';
-import { Text, View, FlatList, TouchableOpacityBase } from 'react-native';
+import { Text, View, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
-// Internal imports
-import styles from '../styles';
+const renderItem = ({ item, index }) => {
+    const buttonStyle = index === 0 ? styles.firstButton : styles.button;
 
-export default function NextTaskScreen() {
     return (
-      <View style={styles.container}>
-        <Text>Settings ciao</Text>
-      </View>
+      <TouchableOpacity style={buttonStyle}>
+        <Text style={styles.buttonText}>{item.title}</Text>
+      </TouchableOpacity>
+    );
+};
+
+function NextTaskScreen() {
+    const data = [
+        { id: 1, title: 'Very very very very very very very very very very very very long text' },
+        { id: 2, title: 'Button 2' },
+        { id: 3, title: 'Button 3' },
+        { id: 4, title: 'Button 4' },
+        { id: 5, title: 'Button 5' },
+        { id: 6, title: 'Button 6' },
+        { id: 7, title: 'Button 7' },
+    ];
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                contentContainerStyle={styles.flatList}
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 10,
+    },
+    flatList: {
+        flexGrow: 1,
+    },
+    button: {
+        flex: 1,
+        backgroundColor: 'blue',
+        padding: 20,
+        marginVertical: 10,
+        borderRadius: 10,
+    },
+    firstButton: {
+        flex: 1,
+        backgroundColor: 'red',
+        paddingVertical: 100,
+        paddingHorizontal: 20,
+        marginVertical: 10,
+        borderRadius: 10,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+  });
+
+  export default NextTaskScreen;
