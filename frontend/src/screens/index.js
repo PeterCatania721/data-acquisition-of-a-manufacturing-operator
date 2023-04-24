dd532831712019f33698c4902cc570286e5ae185
Byketa Marsildo <marsildo.byketa@student.supsi.ch>
2023-04-24 13:58:46 +0200

    new push

--- frontend/src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export {default as HomeScreen} from './HomeScreen';
-export {default as FatigueListScreen} from './FatigueListScreen';
-export {default as FatigueDoubleListScreen} from './FatigueDoubleListScreen';
-export {default as NextTaskScreen} from './NextTaskScreen';
