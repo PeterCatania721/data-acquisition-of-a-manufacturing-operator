495bd0c08086078288ace3ac2bde4811fa0a10f0
Byketa Marsildo <marsildo.byketa@student.supsi.ch>
2023-04-27 10:54:07 +0200

    notifiche check internet

--- Expo-FaMS-Labelling/App.js
+++ Expo-FaMS-Labelling/App.js
@@ -1,9 +1,46 @@
-// Internal imports
-import HomeNavigation from './src/navigation/HomeNavigation';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import NetInfo from '@react-native-community/netinfo';
 
 export default function App() {
+
+  const [data, setData] = useState(null);
+
+  NetInfo.addEventListener(state => {
+    if (state.isConnected) {
+      console.log('Il dispositivo è connesso a internet');
+    } else {
+      console.log('Il dispositivo non è connesso a internet');
+    }
+  });
+
+    
+  React.useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+    return () => subscription.remove();
+  }, []);
+
   return (
-    <HomeNavigation>
-    </HomeNavigation>
+    <View style={styles.container}>
+      <Text style={styles.text}>ciao</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
--- Expo-FaMS-Labelling/src/utils/constants.js
+++ Expo-FaMS-Labelling/src/utils/constants.js
@@ -1,6 +1,6 @@
 
 const Constants = {
-    API_BASE_URL: 'http://172.20.10.2:4000/api/v1/',
+    API_BASE_URL: 'http://localhost:4000/api/v1/',
 }
 
 export default Constants;
